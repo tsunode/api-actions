7075e0cca9aa2824ea09e7c013871e7ea4a3908d
tsunodajapa <gabrieltsunoda@gmail.com>
2023-07-14 00:04:21 -0300

    d

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => res.json('okkkkkk'))
+app.get('/', (req, res) => res.json('okkkkkkkkkkk Foi em'))
 
 app.listen(21123)
